78897a1134161bd754ce92f09e85c9c1a4b53950
1447bits <untilinfinity00@gmail.com>
2023-10-22 13:31:35 +0530

    create gallery section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import AppHero from "./components/AppHero";
 // import Events from "./components/Events";
+import Gallery from "./components/Gallery";
 import "./styles/App.css"
 
 function App() {
   return (
     <div id="App">
-      <AppHero />
+      {/* <AppHero /> */}
+      <Gallery />
       {/* <Events /> */}
     </div>
   );
